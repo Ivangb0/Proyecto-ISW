fbbefcd355e29907cb5016389b44989385a6c3ee
Rubén García García-Nieto <RGARGAR@upv.edu.es>
2022-10-13 12:31:35 +0200

    Paper por acabar

--- ProyectoPracticas/ClassLibrary/BusinessLogic/Entities/Paper.cs
+++ ProyectoPracticas/ClassLibrary/BusinessLogic/Entities/Paper.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Magazine.Entities
 {
-    public partial class Paper
+    //asocicion unidireccional
+    public partial class Paper : Evaluation  
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public UploadDate DateTime { get; set; }
+        public virtual ICollection<Person> CoAuthors { get; set; }
+        public virtual User Resiponble { get; set; }
+        public virtual Area BelongingArea { get; set; }
+        public virtual Area PublicationPendingArea { get; set; }
+
     }
 }
